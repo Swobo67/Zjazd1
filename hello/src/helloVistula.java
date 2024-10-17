20bc2af2815a670e7aded8c0db405b709898265e
Swobodny <aswobod2@stu.vistula.edu.pl>
2024-10-17 12:56:10 +0200

    five commit

--- hello/src/helloVistula.java
+++ hello/src/helloVistula.java
@@ -2,5 +2,6 @@ public class helloVistula {
     public static void main(String[] args) {
         System.out.println("Hello Vistula");
         System.out.println("Hello World");
+        System.out.println("Hello");
     }
 }
